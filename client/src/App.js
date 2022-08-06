73c5747ea0e93ffea24793cfab8d926e20b907af
Samvarthana <samvarthanag.20cse@kongu.edu>
2022-08-06 09:22:31 +0530

    email page commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+const emailValidation = () => {
+  const regEx = /([a-zA-Z]+)[\.]([0-9]+)([a-z0-9]+)@kongu([.])edu/
+  if(regEx.test(email)){
+    setMessage("Email is Valid")
+  } else if(!regEx.test(email)&& email !==' '){
+    setMessage("Email is not valid");
+  } else{
+    setMessage(" ");
+  }
+};
+
+const handleOnChange = (e) => {
+  setEmail(e.target.value);
+};
+
+return(
+  <div className = "app">
+    <div className = "header">
+      <h2>Email Page</h2>
     </div>
+    <div className = "card">
+      <label htmlFor="email" className="label">Email:</label>
+      <input id = "email" 
+      className = "input" type = "email" placeholder="abc.20cse@kongu.edu" value = {email} 
+      onChange = {handleOnChange}/>
+      <div className = "button">
+      <button className = "button" onClick = {emailValidation}>Proceed to OTP Generation</button>
+      </div>
+      <p className = "message">{message}</p>
+    </div>
+  </div>
   );
 }
 
