6cc73c5388d28975d1f54cd45854a734bd9874a8
NivethaVadivel <nivethavadivel.1@gmail.com>
2022-08-07 21:41:50 +0530

    home second commit

--- client/src/components/InfoSection/index.js
+++ client/src/components/InfoSection/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../ButtonElements';
 
 import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElements';
 
 import { homeObjOne } from './Data';
-const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, buttonLabel, img, alt}) => {
+const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, buttonLabel, img, alt, primary, dark, dark2}) => {
 
 console.log(homeObjOne);
+const[image,setImage] = useState(null)
+useEffect(() => {
+  setImage(require('../../images/'+homeObjOne.img))
+
+  
+}, [])
 
     
   return (
@@ -15,20 +21,20 @@ console.log(homeObjOne);
  
      <InfoContainer lightBg={homeObjOne.lightBg} id={homeObjOne.id}>
         <InfoWrapper>
-            <InfoRow imgStart={'imgStart'}> 
+            <InfoRow imgStart={homeObjOne.imgStart}> 
                 <Column1>
                     <TextWrapper>
                         <TopLine>{homeObjOne.topLine}</TopLine>
                           <Heading lightText={homeObjOne.lightText}>{homeObjOne.headLine}</Heading>
                            <Subtitle darkText={homeObjOne.darkText}>{homeObjOne.description}</Subtitle>
                         <BtnWrap>
-                            <Button to="home">{homeObjOne.buttonLabel}</Button>
+                            <Button to="home" smooth={true} duration={500} spy={true} exact={true} offset={-80} primary={homeObjOne.primary ? 1 : 0} dark={homeObjOne.dark ? 1 : 0} dark2={homeObjOne.dark2 ? 1 : 0}>{homeObjOne.buttonLabel}</Button>
                         </BtnWrap> 
                     </TextWrapper>
                 </Column1>
                 <Column2>
                     <ImgWrap>
-                        <Img src={homeObjOne.img} alt={homeObjOne.alt}/>
+                        <Img src={image} alt={homeObjOne.alt}/>
                     </ImgWrap>
                 </Column2> 
             </InfoRow>
