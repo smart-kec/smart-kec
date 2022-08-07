6b51faaada15af2f9b6fd780be62403fada48030
AANANDHENE M <91001392+Aanandhene@users.noreply.github.com>
2022-08-07 09:35:02 +0530

    dashboard created

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Deptform from "./components/form/Deptform";
 
-function App() {
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      {/* <h1>Smart KEC</h1> */}
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+      <Deptform />
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/form/Deptform.js
@@ -0,0 +1,9 @@
+import "./Deptform.css";
+const Deptform = ({}) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+export default Deptform;
