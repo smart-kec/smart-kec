a0397451ef308901d3edbba720028c46aa36e9f3
NeerajMayilvakanan <89233866+NeerajMayilvakanan@users.noreply.github.com>
2022-08-12 20:01:07 +0530

    ff

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import EmailPage from "./components/email/EmailPage";
 import SignIn from "./components/Signin";
 import Navbar from "./components/Navbar";
-import Organiserinfo from './components/Organizer_Info/organiser';
+import Organiserinfo from "./components/Organizer_Info/organiser";
 
 function App() {
-    // const [isOpen, setIsOpen] = useState(false);
-  
-    // const toggle = () => {
-    //   setIsOpen(!isOpen);
-    // };
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const toggle = () => {
+  //   setIsOpen(!isOpen);
+  // };
   return (
     <>
-    {/* <Navbar toggle={toggle} />
+      {/* <Navbar toggle={toggle} />
     <h1>hi</h1> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/email" element={<EmailPage />} />
         <Route path="/login/admin" element={<SignIn />} />
-        <Route path="/admin/organizer/add" element={<Organiserinfo />} />
+        {/* <Route path="/admin/organizer/add" element={<Organiserinfo />} /> */}
       </Routes>
     </>
   );
