041ba21cba5576c9e74c09b2a939234cda547b0e
AANANDHENE M <91001392+Aanandhene@users.noreply.github.com>
2022-08-15 00:02:37 +0530

    department form changed and error message added

--- client/src/components/form/Deptform.js
+++ client/src/components/form/Deptform.js
@@ -1,88 +1,29 @@
 import "./Deptform.css";
-const Deptform = ({}) => {
+import { useState } from "react";
+const Deptform = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div class="container-div">
-      <div class="title">Department Details</div>
-      <div class="content">
-        <form action="#">
-          <div class="user-details">
-            <div class="input-box">
-              <span class="details">Department Full Name</span>
-              <input
-                type="text"
-                placeholder="Enter Department Name"
-                required
-              ></input>
-            </div>
-            <div class="input-box">
-              <span class="details">Department Alias Name</span>
-              <input
-                type="text"
-                placeholder="Enter Alias Name"
-                required
-              ></input>
-            </div>
-            <div class="input-box">
-              <span class="details"> Department Email</span>
-              <input
-                type="text"
-                placeholder="Enter Department Email"
-                required
-              ></input>
-            </div>
-
-            <div class="input-box">
-              <span class="details">Password</span>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                required
-              ></input>
-            </div>
-            <div class="input-box">
-              <span class="details">Number of Semesters</span>
-              <input
-                type="number"
-                placeholder="Enter no of Semesters"
-                required
-              ></input>
-            </div>
-            <div class="input-box">
-              <span class="details"> HOD Email</span>
-              <input type="text" placeholder="Enter HOD Email" required></input>
-            </div>
-            <div class="input-box">
-              <span class="details">No of Faculties</span>
-              <input
-                type="number"
-                placeholder="Enter no of Faculties"
-                required
-              ></input>
-            </div>
-            <div class="input-box">
-              <span class="details">No of Students</span>
-              <input
-                type="number"
-                placeholder="Enter no of Students"
-                required
-              ></input>
-            </div>
-            {/* <div class="input-box">
-              <span class="details">Year-Incharge Name</span>
-              <input
-                type="text"
-                placeholder="Enter Year-Incharge Name"
-                required
-              ></input>
-            </div> */}
-          </div>
-
-          <div class="button">
-            <input type="submit" value="Submit"></input>
-          </div>
-        </form>
-      </div>
+    <div className="formInput">
+      <label className="dept-label">{label}</label>
+      <input
+        className="dept-input"
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span className="dept-span">{errorMessage}</span>
     </div>
   );
 };
+
 export default Deptform;
