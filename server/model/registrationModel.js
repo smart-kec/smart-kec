73c13d5a6d227d2a8c9ae3e5b43529a594d12ba7
NeerajMayilvakanan <89233866+NeerajMayilvakanan@users.noreply.github.com>
2022-08-27 05:44:09 +0530

    Models change

--- server/model/registrationModel.js
+++ server/model/registrationModel.js
@@ -1,75 +1,22 @@
-
 const mongoose = require("mongoose");
 
-
-const registeredStudentsInfoSchema = new mongoose.Schema({
-  regStudentRollNo: {
-    type: [String],
-    default: [],
-  },
-
-
-  isSelectedParticipant: {
-    type: Boolean,
-    required: [true, "Required : Has Participant selected?"],
-    default: false,
-  },
-  studentDocument: {
-    type: String,
-  },
-});
-
-const selectedParticipantsSchema = new mongoose.Schema({
-  participateStudentRollNo: {
-    type: [String],
-    required: [true, "A registered student must have a roll number"],
-  },
-  participantAttendence: {
-    type: Boolean,
-    required: [true, "Required : Participant Attendence"],
-    default: false,
-  },
-  
-});
-
-const registeredFestEventsSchema = new mongoose.Schema({
-  eventName: {
-    type: String,
-    required: [true, "Fest must contain an Event name"],
-    trim: true,
-  },
-  registeredStudentsInfo: {
-    type: [registeredStudentsInfoSchema],
-    default: [],
-  },
-  selectedParticipantAttendence: {
-    type: [selectedParticipantsSchema],
-    default: [],
-  },
-});
-
 const registrationSchema = new mongoose.Schema({
-
-  registrationFestId: {
+  eventKey: {
     type: String,
-    required: [true, "An event must have an event id"],
-    trim: truemongoose,
   },
-  festName: {
-    type: String,
-    required: [true, "Event name is must for the new Event"],
-    unique: true,
-    trim: true,
-  },
-  festOrganizer: {
-    type: String,
-    required: [true, "Organizer for an event is must"],
-  },
-  registeredFestEvents: {
-    type: [registeredFestEventsSchema],
-    default: [],
-  },
-
+  regstd: [
+    {
+      stdKeys: {
+        type: [String],
+        default: [],
+      },
+      docLink: {
+        type: String,
+      },
+    },
+  ],
+  selStd: [String],
+  participatedStd: [String],
 });
 
 const registrationModel = mongoose.model(
