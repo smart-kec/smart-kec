a499e8bcddda29ce2da855fb8b83375becc849e9
AANANDHENE M <91001392+Aanandhene@users.noreply.github.com>
2022-09-05 21:02:06 +0530

    Loading page

--- client/src/components/LoadingPage/LoadingComponent.js
+++ client/src/components/LoadingPage/LoadingComponent.js
@@ -1,8 +1,16 @@
-<div class="loader">
-  <span class="dot"></span>
-  <div class="dots">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-</div>;
+import React from "react";
+import "./LoadingComponent.css";
+const LoadingComponent = ({}) => {
+  return (
+    <div class="loader">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingComponent;
