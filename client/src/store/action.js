ad43d02ae23bea3a1efcfae6782704c74ee1e571
NeerajMayilvakanan <89233866+NeerajMayilvakanan@users.noreply.github.com>
2022-09-15 06:15:14 +0530

    loading success

--- client/src/store/action.js
+++ client/src/store/action.js
@@ -1,9 +1,15 @@
 export const SET_EMAIL = "SET_EMAIL";
-
+export const LOADING_TOGGLE_ACTION = "LOADING_ACTION";
 export const setData = (email) => {
   return {
     type: SET_EMAIL,
     UserEmail: email,
   };
 };
-export default setData;
+
+export const loadingPage = (status) => {
+  return {
+    type: LOADING_TOGGLE_ACTION,
+    payload: status,
+  };
+};
